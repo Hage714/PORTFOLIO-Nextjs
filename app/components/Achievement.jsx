3a99bcd23e60db634c60c49dcab667508f3d70ce
Hage Woche <66566707+Hage714@users.noreply.github.com>
2024-08-04 12:42:22 +0300

    Changes on responsiveness for achievement section

--- app/components/Achievement.jsx
+++ app/components/Achievement.jsx
@@ -26,15 +26,15 @@ postfix: "+",
 ]
 const Achievement = () => {
   return (
-    <div className='py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-        <div className='border-slate-600 border rounded-md py-8 px-16 flex flex-row items-center justify-between'>
+      <div className='py-8 px-4 sm:py-16 sm:px-8 lg:px-16'>
+          <div className='border-slate-600 border rounded-md py-8 px-16 flex flex-row items-center justify-between space-y-8 md:flex-row md:space-y-0'>
         {achievementsList.map((achievement, index) => {
         return (
             <div key={index} className='flex flex-col items-center justify-center mx-4'>
                 <h2 className='text-black text-4xl font-bold flex flex-row'>
                     {achievement.prefix}
                     <AnimatedNumbers includeComma animateToNumber={parseInt(achievement.value)}
-                        locale='en-US' className='text-black text-4xl font-bold' configs={(_, index) => {
+                        locale='en-US' className='text-black text-3xl sm:text-4xl font-bold' configs={(_, index) => {
                              return {
                                 mass: 1,
                                 friction: 100,
@@ -44,7 +44,7 @@ const Achievement = () => {
                     />
                     {achievement.postfix}
                 </h2>
-                <p className='text-black text-base'>{achievement.metric}</p>
+                <p className='text-black text-base sm:text-lg'>{achievement.metric}</p>
             </div>
         )
       })}
